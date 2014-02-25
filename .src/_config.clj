e425293c5ad3088ba5b11e8cdf5153daded8db5c
mikio_kun <ocha.awk@gmail.com>
2014-02-26 00:59:11 +0900

    org-modeの改行問題の記事を追加した。

--- .src/_config.clj
+++ .src/_config.clj
@@ -18,7 +18,7 @@
  :lang "ja"
 
  :site {:site-title    "ざる魂"
-        :site-subtitle "最初はただの「まね」でいい。真似て学んで「まねぶ」。知られざる我が魂。" 
+        :site-subtitle "真似て学んで「まねぶ」サル。知られざる我が魂。" 
         :your-domain   "mikio.github.io"
         :atom          "atom.xml"
         :atom-base     "http://localhost:8080"
