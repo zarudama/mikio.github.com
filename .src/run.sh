8a6d906082e56836424ca210769a0fdc7922b36d
mikio_kun <ocha.awk@gmail.com>
2014-06-01 13:03:59 +0900

    .src/template/postsへのシンボリックリンクを削除
    リポジトリ上に存在しないシンボリックリンクがあると
    githubがエラーになる仕様になってしまった。
    そのため、run.shで、実行時にシンボリックリンクを張り
    終了時にtrapコマンドでシンボリックリンクを削除するようにした。
    
    	modified:   .src/run.sh
    	deleted:    .src/template/posts

--- .src/run.sh
+++ .src/run.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+
 PWD=`pwd`
 PUBLIC=~/dev/mikio.github.com
 MISAKI=~/dev/misaki-orgmode
 SRC=$PUBLIC/.src
-rm -rf $PUBLIC/*.html
-rm -rf $PUBLIC/*.xml
-rm -rf $PUBLIC/page*
-rm -rf $PUBLIC/tag
-rm -rf $PUBLIC/article
+
+function finally() {
+    echo finally!
+    rm $SRC/template/posts
+    exit 1
+}
+
+function clean() {
+    rm -rf $PUBLIC/*.html
+    rm -rf $PUBLIC/*.xml
+    rm -rf $PUBLIC/page*
+    rm -rf $PUBLIC/tag
+    rm -rf $PUBLIC/article
+}
+
+trap 'finally' 2
+clean
+ln -s ~/Dropbox/org/posts $SRC/template/posts 
 cd $MISAKI;
 #lein run $SRC --compile
 lein run $SRC
